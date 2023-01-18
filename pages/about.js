855bc2a7bd5d77fcf5b91b290f6ae5c8d46e7e06
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-01-18 23:29:15 +0000

    Primeros pasos

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,16 @@
 function About() {
-    return <h1>About page</h1>
+    return( <div>
+   <h1>Nosotros</h1>
+   <p>
+   Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+   Sed semper viverra tortor nec ultricies. 
+   Nam nec arcu id diam vehicula interdum.
+   Phasellus a ligula vel enim mollis interdum quis sit amet nulla. 
+   Nulla sit amet ex sem. Praesent elementum ligula sapien, et consectetur nulla bibendum quis. In pharetra diam id eros tempor, non maximus erat malesuada. Sed in ante ornare odio luctus pharetra.
+   </p>
+
+    </div>
+    );
 }
  
 export default About 
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,40 @@ function HomePage() {
         </nav>
     </header>
 
+ <main className="styles.main">
+    <a href="#">
+    <img />
+    <h2>Machu Pichu</h2>
+    <p>
+    La zona arqueológica es accesible, bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
+
+    </p>
+</a>
+
+    <a href="#">
+    <img />
+    <h2>Galapagos</h2>
+    <p>
+    La zona arqueológica es accesible, bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
+
+    </p>
+    </a>
+
+    
+    <a href="#">
+    <img />
+    <h2>Venezuela</h2>
+    <p>
+    La zona arqueológica es accesible, bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
+
+    </p>
+    </a>
+
+
+
+
+ </main>
+
 
 
    </div>
