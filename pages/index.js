260d3e4b01ae928e7ea6ea949911a0b455200335
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-02-07 18:15:42 +0000

    Styles

--- pages/index.js
+++ pages/index.js
@@ -5,27 +5,33 @@ export default function HomePage() {
     <header>
         <title>Núnka</title>
     </header>
-    <body class="bg-cyan-400">
+  
 
     <header>
 
-        <nav className="text-3xl font-bold underline">
-       
-       <a href="#">NúnKa</a> 
+        <nav class="flex items-center justify-between flex-wrap bg-teal-900 p-6 ">
+        <div class="flex items-center flex-shrink-0 text-white mr-6 "> 
+        <span class="font-italic text-xl tracking-tight">
+      
+       <a href="#">NúnKa</a> </span>
        <a href="/nosotros">Nosotros</a>
        <a href="/viajes">viajes</a>
        <a href="/Imagenes">Imagenes</a>
        <a href="/contacto">Contacto</a> 
+       </div>
         </nav>
+        
+        
     </header>
-
-    </body>
+    
+   
 
     
 
  <main className="styles.main">
- 
 
+
+    
  
 
     <a href="viajes/viaje1">
@@ -33,7 +39,6 @@ export default function HomePage() {
     <h2>Roma</h2>
     <p>
     La zona arqueológica es accesible, bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
-
     </p>
 </a>
 
