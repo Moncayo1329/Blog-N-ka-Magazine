ff5d89787dd357cffb580fbc7dc5afaec362a888
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-02-23 18:48:31 +0000

    desgine

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ export default function HomePage() {
   
 
      <header>
-    <div>
-        <nav class="flex items-center justify-between flex-wrap bg-teal-900 p-6 "> 
-        <div class="flex items-center flex-shrink-0 text-black mr-6 "> 
+    <div class="menu">
+        <nav class="" > 
+        <div class=""> 
        <a href="#">NúnKa</a> 
        <a href="/nosotros">Nosotros</a>
-       <a href="/viajes">viajes</a>
-       <a href="/Imagenes">Imagenes</a>
+       <a href="/viajes">Viajes</a>
+       <a href="/Imagenes">Fotografías</a>
        
 
        </div>
@@ -31,13 +31,13 @@ export default function HomePage() {
 
  <main className="styles.main">
 
-
-    
  
+    
+
 
     <a href="viajes/viaje1">
     <img />
-    <h2>Roma</h2>
+    <h2 className="text-indigo-400">Roma</h2>
     <p>
     La zona arqueológica es accesible, bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
     </p>
@@ -69,15 +69,15 @@ export default function HomePage() {
     <h2>Islas Galapagos</h2>
     <p>
      caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
-    
+     
+     
     </p>
     </a>
     
  </main>
  
- 
-
    </div>
+   
 
 }
  
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,10 +1,28 @@
+import Image from 'next/image';
+import img from "../public/public/images/WEB.jpeg"
+
+
+
+
+
 function nosotros() {
     return( <div>
    <h1>Nosotros</h1>
-   <p>
+   <p class="About">
+
    Esta website fue creada por Michael Moncayo. Me gusta programar en mi tiempo libre. <br></br>
    Construi desde cero esta website con el objetivo de compartir mis viajes y algunos tips que creo que pueden ser util al momento de viajar.
    </p>
+
+   <div className="grid-element">
+    <Image
+      src={img}
+      Layout= "fill"
+      alt= "Picture of florencia"
+     priority
+      
+    />
+  </div>
 
     </div>
     );
