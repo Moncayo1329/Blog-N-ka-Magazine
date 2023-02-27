e7de42c6d5db1194ce3c50607ff013ddbca2dc50
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-02-27 03:34:32 +0000

    Styles and coding

--- pages/index.js
+++ pages/index.js
@@ -10,14 +10,16 @@ export default function HomePage() {
 
      <header>
     <div class="menu">
-        <nav class="" > 
-        <div class=""> 
-       <a href="#">NúnKa</a> 
-       <a href="/nosotros">Nosotros</a>
+        <nav className="#"> 
+        <div class="letter-spacing: -100em flex  space-x-12 text-center">
+        
+       <a href="#" className="text-zinc-900">NúnKa</a> 
+       
        <a href="/viajes">Viajes</a>
        <a href="/Imagenes">Fotografías</a>
+       <a href="/nosotros">Nosotros</a>
        
-
+       
        </div>
        </nav>
         
@@ -37,18 +39,18 @@ export default function HomePage() {
 
     <a href="viajes/viaje1">
     <img />
-    <h2 className="text-indigo-400">Roma</h2>
-    <p>
-    La zona arqueológica es accesible, bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
+    <h2 className="text-neutral-900 font-size:3.75rem  md:text-center inline-block:align-baseline">Roma</h2>
+    <p class="text-center">
+    La zona arqueológica es accesible <br></br> bien desde los caminos post-incaicos<br></br> que llegan hasta ella<br></br>o bien utilizando la carretera Hiram Bingham <br></br>que asciende la cuesta del cerro Machu Picchu.
     </p>
 </a>
 
 
     <a href="#">
     <img />
-    <h2>Florencia</h2>
+    <h2>Roma</h2>
     <p>
-    , bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
+     bien desde los caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
 
     </p>
     </a>
@@ -66,7 +68,7 @@ export default function HomePage() {
     
     <a href="#">
     <img />
-    <h2>Islas Galapagos</h2>
+    <h2>Florencia</h2>
     <p>
      caminos post-incaicos que llegan hasta ella, o bien utilizando la carretera Hiram Bingham (que asciende la cuesta del cerro Machu Picchu desde la antigua estación de tren de Puente Ruinas, ubicada al fondo del cañón). Ninguna de las dos formas exime al visitante del precio de ingreso al complejo.
      
